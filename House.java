982b0a1c3c865c56ed550f8694be31a60038295e
Flow <89105476+Flow-Glow@users.noreply.github.com>
2023-10-02 21:54:56 -0700

    Update House.java

--- House.java
+++ House.java
@@ -1,27 +1,69 @@
 import gpdraw.*;
 
-public class HouseDrawing {
+public class House {
+    private DrawingTool pen = new DrawingTool(new SketchPad(400, 400));
+    private int length = 100;
+
     public static void main(String[] args) {
-        // Create a GPDraw window
-        SketchPad pad = new SketchPad(400, 400);
-        DrawingTool pen = new DrawingTool(pad);
+        House house = new House();
+        house.draw();
+    }
 
-        // Draw the base of the house
-        pen.drawRect(100, 200, 200, 150);
+    public void draw() {
+        drawBase();
+        drawRoof();
+        drawDoor();
+        drawWindows();
+    }
 
-        // Draw the roof
-        pen.drawLine(100, 200, 200, 100);
-        pen.drawLine(200, 100, 300, 200);
+    private void drawBase() {
+        pen.up();
+        pen.move(0, 0);
+        pen.down();
+        pen.forward(length * 2); // Adjust the length to set the base width
+        pen.turnRight(90);
+        pen.forward(length * 1.5); // Adjust the length to set the base height
+        pen.turnRight(90);
+        pen.forward(length * 2);
+        pen.turnRight(90);
+        pen.forward(length * 1.5);
+    }
 
-        // Draw the door
-        pen.drawRect(175, 250, 50, 100);
+    private void drawRoof() {
+        pen.up();
+        pen.move(0, length * 1.5);
+        pen.down();
+        pen.turnLeft(45);
+        pen.forward(length * 2.12); // Adjust the length to set the roof diagonal
+        pen.turnLeft(90);
+        pen.forward(length * 2.12);
+        pen.turnLeft(135);
+    }
 
-        // Draw windows
-        pen.drawRect(125, 125, 50, 50);
-        pen.drawRect(225, 125, 50, 50);
+    private void drawDoor() {
+        pen.up();
+        pen.move(length * 0.75, 0);
+        pen.down();
+        pen.forward(length * 0.5); // Adjust the length to set the door width
+        pen.turnRight(90);
+        pen.forward(length);
+        pen.turnRight(90);
+        pen.forward(length * 0.5);
+    }
 
-        // Close the GPDraw window when done
-        pad.close();
+    private void drawWindows() {
+        // Draw windows on the sides of the house
+        for (int i = 0; i < 2; i++) {
+            pen.up();
+            pen.move(length * 0.2 + i * length * 1.6, length * 0.5);
+            pen.down();
+            pen.drawRect(length * 0.6, length * 0.6); // Adjust the size of the windows
+        }
+
+        // Draw a window on the roof
+        pen.up();
+        pen.move(length * 0.75, length * 1.5);
+        pen.down();
+        pen.drawRect(length * 0.5, length * 0.5); // Adjust the size of the window
     }
 }
-
