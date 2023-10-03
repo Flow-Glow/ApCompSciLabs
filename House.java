2acaea93a671ba445a9f7f3ad0c11b1d8c570365
Flow <89105476+Flow-Glow@users.noreply.github.com>
2023-10-02 22:29:57 -0700

    Update House.java

--- House.java
+++ House.java
@@ -1,49 +1,21 @@
-import gpdraw.SketchPad;
-import gpdraw.DrawingTool;
+import gpdraw.*;
 
-public class DrawHouse {
+public class TurtleDrawing {
     public static void main(String[] args) {
-        // Create a SketchPad object with a 500x500 drawing area
-        SketchPad paper = new SketchPad(500, 500);
+        DrawingTool pen = new DrawingTool(new SketchPad(400, 400)); // Adjust the size as needed
 
-        // Create a DrawingTool object to draw on the SketchPad
-        DrawingTool pen = new DrawingTool(paper);
+        pen.turnRight(90);
+        pen.forward(180);
+        pen.turnLeft(90);
+        pen.forward(180);
+        pen.turnLeft(90);
+        pen.forward(180);
+        pen.turnLeft(90);
+        pen.forward(180);
 
-        // Set the pen's initial position
-        pen.up();
-        pen.move(-50, -50); // Adjust the values to center the house horizontally
-        pen.down();
-
-        // Draw the house's body
-        pen.drawRect(200, 200); // Adjust the values to control the size of the house
-
-        // Draw the sloped roof
-        pen.up();
-        pen.move(0, 200); // Move to the top-left corner of the house
-        pen.down();
-        pen.drawLine(100, 100); // Draw the left side of the roof
-        pen.drawLine(100, -100); // Draw the right side of the roof
-
-        // Draw the door
-        pen.up();
-        pen.move(75, 0); // Move to the center of the house
-        pen.down();
-        pen.drawRect(50, 100); // Adjust the values to control the door's size
-
-        // Draw windows (you can add more if needed)
-        // Window 1
-        pen.up();
-        pen.move(25, 125); // Adjust the coordinates as needed
-        pen.down();
-        pen.drawRect(50, 50); // Adjust the size of the window
-
-        // Window 2
-        pen.up();
-        pen.move(125, 125); // Adjust the coordinates as needed
-        pen.down();
-        pen.drawRect(50, 50); // Adjust the size of the window
-
-        // Close the drawing window when done (optional)
-        paper.close();
+        pen.turnRight(135);
+        pen.forward(120);
+        pen.turnRight(85);
+        pen.forward(130);
     }
 }
