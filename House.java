f5b6cef53a910d110d0edeec7c690a3648e8ce2b
Flow <89105476+Flow-Glow@users.noreply.github.com>
2023-10-02 21:49:45 -0700

    Update House.java

--- House.java
+++ House.java
@@ -1,1 +1,27 @@
+import gpdraw.*;
+
+public class HouseDrawing {
+    public static void main(String[] args) {
+        // Create a GPDraw window
+        SketchPad pad = new SketchPad(400, 400);
+        DrawingTool pen = new DrawingTool(pad);
+
+        // Draw the base of the house
+        pen.drawRect(100, 200, 200, 150);
+
+        // Draw the roof
+        pen.drawLine(100, 200, 200, 100);
+        pen.drawLine(200, 100, 300, 200);
+
+        // Draw the door
+        pen.drawRect(175, 250, 50, 100);
+
+        // Draw windows
+        pen.drawRect(125, 125, 50, 50);
+        pen.drawRect(225, 125, 50, 50);
+
+        // Close the GPDraw window when done
+        pad.close();
+    }
+}
 
